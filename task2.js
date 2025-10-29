1674beb247c3bdb0101d757c00e2766c2e54d614
Maruf <marufalam12ab12@gmail.com>
2025-10-29 20:49:46 +0600

    update task

--- task2.js
+++ task2.js
@@ -1,1 +1,15 @@
 const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+
+function names (heights2){
+    const len = heights2.length;
+    let he = heights2[0];
+    for(heights of heights2){
+        if(heights.length < he.length){
+            he = heights;
+        }
+    }
+    return he;
+}
+
+const result = names(heights2);
+console.log(result);
