50d6663ecb4c1b4c2c91cef92fc444c7b2cf5ad4
Maruf <marufalam12ab12@gmail.com>
2025-10-29 21:17:55 +0600

    maam kakak

---
+++ task3.js
@@ -0,0 +1,7 @@
+function calculateElectronicsBudget (laptop,phone,tablet){
+    let sum = laptop + phone + tablet;
+    return sum;
+}
+const bugate = calculateElectronicsBudget(50000,20000,30000);
+console.log(bugate);
+
